6aa9001b39ba41faeea7774656e09b7a21c27f7a
Iida Kainu <iida.kainu@gmail.com>
2024-01-31 16:37:48 +0200

    Persist Redux state

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { PersistGate } from 'redux-persist/integration/react';
 import { RootNavigator } from './screens/Root.navigator';
 import { Provider } from 'react-redux';
-import { store } from './redux';
+import { persistor, store } from './redux';
 
 export const App: React.FC = () => (
   <Provider store={store}>
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <PersistGate loading={null} persistor={persistor}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </PersistGate>
   </Provider>
 );
--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,9 +1,36 @@
 import { configureStore } from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import { closetSlice } from './closet';
 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, closetSlice.reducer);
+
 export const store = configureStore({
-  reducer: { closet: closetSlice.reducer },
+  reducer: { closet: persistedReducer },
+  // Add ignored actions for Redux Persist usage with Redux Toolkit
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type StoreDispatchType = typeof store.dispatch;
